20a469231e9ec05b2f0657344f6ff91d891462e9
DagonDeGal <pawelmisko35@gmail.com>
2023-02-25 14:04:19 +0100

    Zadanie dzien 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//! <-- negacja  && <-- i || <-- lub
+
+string name = "Ewa";
+bool woman =  true;
+var age = 33;
+
+if (woman == true && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (woman = false && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+
