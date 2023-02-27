f9e96d850641e125667ce014d3aeb34a88d4ac76
DagonDeGal <pawelmisko35@gmail.com>
2023-02-27 15:49:34 +0100

    dodatkowa lekcja

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,8 +1,9 @@
-﻿
+﻿using System.ComponentModel.Design;
 
-int number = 4566;
-string numberIsString = number.ToString();
-char[] letters = numberIsString.ToArray();
+int number = 4594;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
+
 int counter0 = 0;
 int counter1 = 0;
 int counter2 = 0;
@@ -24,7 +25,7 @@ foreach (char letter in letters)
     {
         counter1++;
     }
-    else if (letter == '2')
+    else if (letter == 2)
     {
         counter2++;
     }
@@ -32,7 +33,7 @@ foreach (char letter in letters)
     {
         counter3++;
     }
-    else if (letter == '4')
+    else if (letter =='4')
     {
         counter4++;
     }
@@ -51,27 +52,21 @@ foreach (char letter in letters)
     else if (letter == '8')
     {
         counter8++;
-    }
+    }    
     else if (letter == '9')
     {
         counter9++;
     }
-}
 
-    {
-        Console.WriteLine("Analized number is:" + number);
-        Console.WriteLine("0 => " + counter0);
-        Console.WriteLine("1 => " + counter1);
-        Console.WriteLine("2 => " + counter2);
-        Console.WriteLine("3 => " + counter3);
-        Console.WriteLine("4 => " + counter4);
-        Console.WriteLine("5 => " + counter5);
-        Console.WriteLine("6 => " + counter6);
-        Console.WriteLine("7 => " + counter7);
-        Console.WriteLine("8 => " + counter8);
-        Console.WriteLine("9 => " + counter9);
     }
-
-
-
-
+Console.WriteLine("Analized number is:" + number);
+Console.WriteLine("0 => " + counter0);
+Console.WriteLine("1 => " + counter1);
+Console.WriteLine("2 => " + counter2);
+Console.WriteLine("3 => " + counter3);
+Console.WriteLine("4 => " + counter4);
+Console.WriteLine("5 => " + counter5);
+Console.WriteLine("6 => " + counter6);
+Console.WriteLine("7 => " + counter7);
+Console.WriteLine("8 => " + counter8);
+Console.WriteLine("9 => " + counter9);
