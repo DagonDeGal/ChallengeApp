7f25c0df65d4a5616ff4805f4649e190632e8f6a
DagonDeGal <pawelmisko35@gmail.com>
2023-03-02 09:55:05 +0100

    Testy Zadania Domowe 7

---
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeApp.Tests
+{
+    public class EmployeeTests
+    {
+        [Test]
+        public void StatisticMinimumValue()
+        {
+            var employee = new Employee("Marta", "Kowalska", "23");
+            employee.AddGrade(8);
+            employee.AddGrade(7);
+            employee.AddGrade(9);
+
+            var statistic = employee.GetStatistics();
+            Assert.AreEqual(statistic.Min, 7);
+        }
+
+        [Test]
+        public void StatisticMaximuxValue()
+        {
+            var employee = new Employee("Marta", "Kowalska", "23");
+            employee.AddGrade(8);
+            employee.AddGrade(7);
+            employee.AddGrade(9);
+
+
+            var statistic = employee.GetStatistics();
+            Assert.AreEqual(statistic.Max, 9);
+        }
+
+        [Test]
+        public void StatisticAverageValue()
+        {
+            var employee = new Employee("Marta", "Kowalska", "23");
+            employee.AddGrade(8);
+            employee.AddGrade(7);
+            employee.AddGrade(9);
+
+
+            var statistic = employee.GetStatistics();
+
+            Assert.AreEqual(statistic.Average, 8);
+        }
+    }
+}
