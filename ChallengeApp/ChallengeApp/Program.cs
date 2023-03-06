e87bd239a572af917fe0079ece86b9d0808fab90
DagonDeGal <pawelmisko35@gmail.com>
2023-03-06 16:30:28 +0100

    Testy Zadania Domowe 7

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -8,7 +8,13 @@ employee.AddGrade(0.873);
 employee.AddGrade(6);
 employee.AddGrade(2);
 employee.AddGrade('5');
+
 var statistics = employee.GetStatistics();
+var statistics1 = employee.GetStatisticsWithForEach();
+var statistics2 = employee.GetStatisticsWithFor();
+var statistics3 = employee.GetStatisticsWithDoWhile();
+var statistics4 = employee.GetStatisticsWithWhile();
+
 Console.WriteLine($"Average: {statistics.Average:N2}");
 Console.WriteLine($"Min: {statistics.Min}");
 Console.WriteLine($"Max: {statistics.Max}");
